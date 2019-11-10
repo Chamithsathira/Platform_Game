d6d15ad5147bb30a8b5bf9cfc30719b5b7e10f96
Chamithsathira <37792846+Chamithsathira@users.noreply.github.com>
2019-11-10 18:57:03 +0530

    Add files via upload

---
+++ PauseGame.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour {
+
+	public bool gamePaused = false;
+	public AudioSource levelMusic;
+	public GameObject pauseMenu;
+	public AudioSource pauseSound;
+
+
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Cancel")) 
+		{
+			pauseSound.Play ();
+			if (gamePaused == false) {
+				Time.timeScale = 0;
+				gamePaused = true;
+				Cursor.visible = true;
+				levelMusic.Pause ();
+				pauseMenu.SetActive (true);
+			} 
+			else 
+			{
+				pauseMenu.SetActive (false);
+				levelMusic.UnPause ();
+				Cursor.visible = false;
+				gamePaused = false;
+				Time.timeScale = 1;
+			}
+		}
+	}
+
+	public void ResumeGame()
+	{
+		pauseMenu.SetActive (false);
+		levelMusic.UnPause ();
+		Cursor.visible = false;
+		gamePaused = false;
+		Time.timeScale = 1;
+	}
+
+	public void RestartLevel()
+	{
+		pauseMenu.SetActive (false);
+		levelMusic.UnPause ();
+		Cursor.visible = false;
+		gamePaused = false;
+		Time.timeScale = 1;
+		SceneManager.LoadScene (2);
+	}
+
+	public void QuitToMenu()
+	{
+		pauseMenu.SetActive (false);
+		levelMusic.UnPause ();
+		Cursor.visible = false;
+		gamePaused = false;
+		Time.timeScale = 1;
+		SceneManager.LoadScene (1);
+	}
+
+ 
+}
